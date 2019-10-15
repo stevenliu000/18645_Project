178d58b75aa13c04d3c8ec418af9b4ca43d3a8e3
Steven <wxacb147@hotmail.com>
2019-10-15 13:41:09 -0400

    Initial Commit

---
+++ 18645_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  18645_Project
+//
+//  Created by Steven Liu on 10/15/19.
+//  Copyright © 2019 Steven Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
